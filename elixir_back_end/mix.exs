cbb94c51a812a0e216331b48c787be34d3b1d0c1
NtwaliHeritier <heritierntwali@gmail.com>
2021-07-02 07:59:43 +0200

    add package to fetch external api

--- elixir_back_end/mix.exs
+++ elixir_back_end/mix.exs
@@ -39,7 +39,9 @@ defmodule ElixirBackEnd.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:httpoison, "~> 1.8"},
+      {:poison, "~> 4.0"}
     ]
   end
 
